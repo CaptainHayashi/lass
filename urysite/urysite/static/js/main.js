13f49480bbf55f87a6c0872919c8d69b36124a7f
Andrew Durant <aj-dj@tiscali.co.uk>
2012-09-11 00:48:37 +0100

    banner django + rotation

--- urysite/urysite/static/js/main.js
+++ urysite/urysite/static/js/main.js
@@ -1,1 +1,11 @@
+// JS for URY website
+
+// Run this JS when the document is ready
+$(document).ready(function() {
+  // Handler for .ready() called.
+  
+  // Banner rotation
+    $('#banner').cycle();
+  
+});
 
