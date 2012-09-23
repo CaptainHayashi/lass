3d9a4a8b83c5ecc43b3753489a678777e23ffe69
Andrew Durant <aj-dj@tiscali.co.uk>
2012-09-23 21:22:05 +0100

    exception raising fix?

--- urysite/website/templatetags/embedded_views.py
+++ urysite/website/templatetags/embedded_views.py
@@ -30,7 +30,7 @@ class ViewNode(Node):
         try:
             if callable(view):
                 return view(context['request'], *args, **kwargs).content
-            raise Exception, "%r is not callable" % view
+            raise Exception, "view is not callable"
         except:
             if settings.TEMPLATE_DEBUG:
                 raise
