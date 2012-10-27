4807309ecd4e314f544a54437922b0383bf17ed8
Matt "Hayashi" Windsor <matt.windsor@ury.york.ac.uk>
2012-10-28 00:52:35 +0100

    beginnings of dynamic home page block system

--- urysite/website/models/__init__.py
+++ urysite/website/models/__init__.py
@@ -1,5 +1,13 @@
+"""
+Models for the website-specific areas of LASS.
+
+"""
+
 from website.models.banner import Banner, BannerType
 from website.models.banner import BannerCampaign, BannerLocation
 from website.models.banner import BannerTimeslot
+
+from website.models.grid import GridBlock
+
 # v-- Temporary
 from website.models.sis_comm import SISComm
---
+++ urysite/website/models/grid.py
@@ -0,0 +1,62 @@
+"""
+Models for the home page grid system.
+"""
+
+from django.db import models
+from metadata.models import Type
+from uryplayer.models import PodcastChannel
+from urysite import model_extensions as exts
+
+
+class GridBlock(Type):
+    """
+    A block in the home page grid.
+
+    Grid blocks are akin to widgets or dashboard items in other
+    analogous systems.
+
+    `GridBlock`s do not contain any positioning information; the
+    `GridBlockLayout` and `GridLayout` models are concerned with
+    this.
+
+    """
+    width = models.IntegerField(
+        help_text='The width of this block, in arbitrary units.'
+    )
+    height = models.IntegerField(
+        help_text='The height of this block, in arbitrary units.'
+    )
+    view_name = models.CharField(
+        max_length=255,
+        help_text='The name of the view to embed in this block.'
+    )
+    more_url = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text='If given, the name of a Django URL to link to '
+        'in the block footer as a source of more information '
+        'on the subject of the contents of this block. '
+    )
+    podcast_channel = models.ForeignKey(
+        PodcastChannel,
+        null=True,
+        blank=True,
+        help_text='If given, a podcast channel that should be linked '
+        'to this block.'
+    )
+    ajax_poll_duration = models.IntegerField(
+        help_text='The number of seconds between each AJAX poll if '
+        'an `ajax_url` is provided.  If 0, AJAX is disabled.'
+    )
+    ajax_url = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text='If given, the name of a Django URL to replace the '
+        'block contents with every `ajax_poll_duration` seconds.'
+    )
+
+    class Meta(Type.Meta):
+        db_table = 'grid_block'
+        app_label = 'website'
+
+    id = exts.primary_key_from_meta(Meta)
