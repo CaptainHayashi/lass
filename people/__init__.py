d857788ff774e9599f89b246886828c8d5d4741c
Matt "Hayashi" Windsor <matt.windsor@ury.york.ac.uk>
2012-12-08 23:48:50 +0000

    BEGONE

--- people/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Services related to people, roles and permissions.
-
-"""
--- uryplayer/__init__.py
+++
@@ -1,4 +0,0 @@
-"""The URY Player system, which provides external access to URY's
-non-live media (at time of writing: podcasts).
-
-"""
